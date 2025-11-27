3eff3d71d50b0d2ec9cfbdddc49664789cb2b10c
Javohir Hayitboyev <javohirhayitboyev@gmail.com>
2025-11-27 12:25:00 +0500

    done

---
+++ models/src/onStart.js
@@ -0,0 +1,18 @@
+import User from "./models/User.js";
+
+async function onStart(chatId, firstName) {
+
+    console.log("onStart...!", chatId);
+
+      const userExists = await User.findOne({ telegramId: chatId });
+
+        console.log(!userExists)};
+
+         if (!userExists) {
+    const newUser = new User({
+      telegramId: chatId,
+      firstname: firstName,
+    });
+
+    newUser.save();
+  }
