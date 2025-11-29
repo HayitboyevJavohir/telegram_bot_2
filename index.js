4b41ef96106cea01c90a83073a0b3bbfbcc46cf0
Javohir Hayitboyev <javohirhayitboyev@gmail.com>
2025-11-29 21:09:58 +0500

    done

--- index.js
+++ index.js
@@ -1,30 +1,84 @@
+// import TelegramBot from "node-telegram-bot-api";
+import TelegramBot from "node-telegram-bot-api";
+import { config } from "dotenv";
+import { onElse } from "./src/onElse.js";
+import { onCourses } from "./src/onCourse.js";
 import { onStart } from "./src/onStart.js";
-// import { onCourses } from "./src/onCourses.js";
+import { onRegister } from "./src/onRegister.js";
 import mongoose from "mongoose";
 import User from "./src/models/User.js";
 import onUsers from "./src/onUsers.js";
-import { config } from "dotenv";
-
-
 
 config();
 
+const TOKEN = process.env.BOT_TOKEN;
 
+const bot = new TelegramBot(TOKEN, { polling: true });
 
+// db connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("db is connected...");
+  })
+  .catch(() => {
+    console.log(`Error: db is not connected...!`);
+  });
 
 bot.on("message", (msg) => {
+  // console.log(msg);
   const chatId = msg.chat.id;
   const text = msg.text;
+  const firstName = msg.chat.first_name;
 
-  if (text === "📚 Kurslar") {
+  if (text == "/start") {
+    onStart(chatId, firstName);
+  } else if (text == "📚 Kurslar") {
     onCourses(chatId);
-  } else if (text === "✍️ Ro‘yxatdan o‘tish") {
+  } else if (text == "✍️ Ro‘yxatdan o‘tish") {
     onRegister(chatId);
-  } else if (text === "/users") {
+  } else if (text == "/users") {
     onUsers(chatId);
   } else {
     onElse(chatId);
   }
 });
 
-export { bot }
+bot.on("callback_query", (query) => {
+  console.log(query);
+  const chatId = query.message.chat.id;
+  const firstName = query.message.chat.first_name;
+  const data = query.data;
+
+  if (data == "course_english") {
+    bot.sendMessage(
+      chatId,
+      `
+    🇬🇧 Ingliz tili kursi haqida:
+
+📆 Davomiyligi: 3 oy  
+⏰ Darslar: Haftasiga 3 marta (1,5 soatdan)  
+👨‍🏫 O‘qituvchi: Tajribali filologlar  
+💰 Narxi: 450 000 so‘m / oy
+
+✍️ Agar sizni bu kurs qiziqtirsa, “Ro‘yxatdan o‘tish” tugmasini bosing.
+
+    `,
+      {
+        reply_markup: {
+          keyboard: [
+            [{ text: "✍️ Ro‘yxatdan o‘tish" }],
+            [{ text: "⬅️ Orqaga" }],
+          ],
+          resize_keyboard: true,
+        },
+      }
+    );
+  }
+
+  // bot.sendMessage(chatId, data);
+});
+
+console.log("Bot ishga tushdi");
+
+export { bot };
--- src/onStart.js
+++ src/onStart.js
@@ -1,7 +1,6 @@
 import { bot } from "../index.js";
 import User from "./models/User.js";
 
-
 async function onStart(chatId, firstName) {
   console.log("onStart...!", chatId);
 
@@ -10,7 +9,7 @@ async function onStart(chatId, firstName) {
   console.log(!userExists);
 
   if (!userExists) {
-
+    // malumotlar bazasida yangi foydalanuvchi yaratish
     const newUser = new User({
       telegramId: chatId,
       firstname: firstName,
@@ -20,5 +19,31 @@ async function onStart(chatId, firstName) {
   }
 
   bot.sendMessage(
-    chatId,)}
-    
+    chatId,
+    `
+  👋 Assalomu alaykum, ${firstName}!
+
+📚 100x o‘quv markazining rasmiy botiga xush kelibsiz!
+
+Bu bot orqali siz:
+• Kurslarimiz haqida batafsil ma’lumot olasiz  
+• Kurslarga onlayn ro‘yxatdan o‘tishingiz mumkin  
+• Jadval va to‘lovlar haqida ma’lumot olasiz  
+
+Quyidagi menyudan kerakli bo‘limni tanlang 👇
+
+  `,
+    {
+      reply_markup: {
+        keyboard: [
+          [{ text: "📚 Kurslar" }, { text: "✍️ Ro‘yxatdan o‘tish" }],
+          [{ text: "ℹ️ Markaz haqida" }, { text: "💬 Fikr bildirish" }],
+          [{ text: "❓ Yordam" }],
+        ],
+        resize_keyboard: true,
+      },
+    }
+  );
+}
+
+export { onStart };
